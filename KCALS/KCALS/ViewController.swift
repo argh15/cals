0b04d800b0ac7419868ce394965c6fec3158adaa
Arghadeep <archakraborty@teksystems.com>
2021-02-22 21:16:32 +0530

    Base commit. Initial project setup.

---
+++ KCALS/KCALS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KCALS
+//
+//  Created by Arghadeep  on 22/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
